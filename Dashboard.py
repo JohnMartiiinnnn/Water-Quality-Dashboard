493b09b1943c348d0786700e3bc771330406c618
John Martin Ejercitado <113336657+JohnMartiiinnnn@users.noreply.github.com>
2025-05-26 22:44:00 +0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -57,16 +57,16 @@ try:
 except FileNotFoundError:
     st.warning("taalinfo.txt not found.")
 
-#hide_streamlit_style = """
-#    <style>
-#        /* Hide the Streamlit header and menu */
-#        header {visibility: hidden;}
-#        /* Optionally, hide the footer */
-#        .streamlit-footer {display: none;}
-#        /* Hide your specific div class, replace class name with the one you identified */
-#    </style>
-#    """
-#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+    <style>
+        /* Hide the Streamlit header and menu */
+        header {visibility: hidden;}
+        /* Optionally, hide the footer */
+        .streamlit-footer {display: none;}
+        /* Hide your specific div class, replace class name with the one you identified */
+    </style>
+    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # ==== CSS STYLING ====
 font_style = f"""
